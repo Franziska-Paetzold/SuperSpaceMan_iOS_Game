bceac2881f9625bc32bc1701ceea3d94f0354b26
Franziska Paetzold <s0554620@htw-berlin.de>
2018-06-04 16:29:58 +0200

    add some physics to the player

--- SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/GameScene.swift
@@ -19,14 +19,20 @@ class GameScene: SKScene {
     
     override init(size: CGSize){
         super.init(size: size)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.1)
+        
         //configuration background
         backgroundNode.size.width = frame.size.width //sets the Node to the views frame
+        
+        //position is a point in the view and anchorPoint is the a relative point in the picture that adjusts to the position point
         backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0) //on which point the node is set (0.5 & 0 is bottom center of the node)
         backgroundNode.position = CGPoint (x: size.width / 2.0, y: 0.0) //set node to the middle and the bottom of the scene
         addChild(backgroundNode) // adds node to scene
         
         //configuration player
-        playerNode.position = CGPoint(x: size.width/2.0, y: 80.0) // anchorPoint is the middle 
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
+        playerNode.physicsBody?.isDynamic = true
+        playerNode.position = CGPoint(x: size.width/2.0, y: 80.0) // anchorPoint is the middle
         addChild(playerNode)
         
     }
