4ae49b4143cd29323a2d52c00122331ed3a3166e
Franziska Paetzold <s0554620@htw-berlin.de>
2018-06-05 13:59:17 +0200

    add power up (in this case the sea shell)

--- SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/GameScene.swift
@@ -12,7 +12,12 @@ class GameScene: SKScene {
     
     let backgroundNode = SKSpriteNode(imageNamed: "background")
     let playerNode = SKSpriteNode(imageNamed: "player")
-    let brick1Node = SKSpriteNode(imageNamed: "brick1")
+    //let brick1Node = SKSpriteNode(imageNamed: "brick1")
+    let seaShellNode = SKSpriteNode(imageNamed: "seashell")
+    
+    let collisionCategoryPlayer : UInt32 = 0x1 << 1
+    let collisionCategoryPowerUpShells : UInt32 = 0x1 << 2
+    
     
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
@@ -20,12 +25,15 @@ class GameScene: SKScene {
     
     override init(size: CGSize){
         super.init(size: size)
+        // needed for extension
+        physicsWorld.contactDelegate = self
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.1)
         
         //ability for user to apply an impulse
         isUserInteractionEnabled = true
         
-        //configuration background
+        
+        //============configuration background==============
         backgroundNode.size.width = frame.size.width //sets the Node to the views frame
         
         //position is a point in the view and anchorPoint is the a relative point in the picture that adjusts to the position point
@@ -33,7 +41,7 @@ class GameScene: SKScene {
         backgroundNode.position = CGPoint (x: size.width / 2.0, y: 0.0) //set node to the middle and the bottom of the scene
         addChild(backgroundNode) // adds node to scene
         
-        //configuration player
+        //============configuration player============
         playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
         playerNode.physicsBody?.isDynamic = true
         playerNode.position = CGPoint(x: size.width/2.0, y: 80.0) // anchorPoint is the middle
@@ -41,18 +49,46 @@ class GameScene: SKScene {
         playerNode.physicsBody?.linearDamping = 1.0
         //stop rotation by collision
         //playerNode.physicsBody?.allowsRotation = false
+        playerNode.physicsBody?.categoryBitMask = collisionCategoryPlayer
+        playerNode.physicsBody?.contactTestBitMask = collisionCategoryPowerUpShells
+        //playerNode.physicsBody?.collisionBitMask = 0 //handle collision on our own s
         addChild(playerNode)
         
+        //============configuration brick1Node============
+        /*
         brick1Node.position = CGPoint(x:150.0, y: size.height - 25.0)
         brick1Node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: brick1Node.size.width, height: brick1Node.size.height))
-        brick1Node.physicsBody = SKPhysicsBody(circleOfRadius: brick1Node.size.width / 2)
+        //brick1Node.physicsBody = SKPhysicsBody(circleOfRadius: brick1Node.size.width / 2)
         brick1Node.physicsBody?.isDynamic = false
         addChild(brick1Node)
+        */
         
+        //============configuration seaShellNode============
+        seaShellNode.name = "SEA_SHELL"
+        seaShellNode.position = CGPoint(x: 150.0, y: size.width - 25)
+        seaShellNode.physicsBody = SKPhysicsBody(circleOfRadius: seaShellNode.size.width / 2)
+        seaShellNode.physicsBody?.isDynamic = false
+        seaShellNode.physicsBody?.categoryBitMask = collisionCategoryPowerUpShells
+        //seaShellNode.physicsBody?.collisionBitMask = 0 //0 = we dont want the program to handle the collision for us
+        addChild(seaShellNode)
     }
     
     //needs isUserInteractionEnabled = true in initializer
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         playerNode.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
+    }
+    
+    
+    
+}
+
+extension GameScene: SKPhysicsContactDelegate{
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("There has been contact")
+        
+        let contactNode = contact.bodyB.node
+        if contactNode?.name == "SEA_SHELL" {
+            contactNode?.removeFromParent()
+        }
     }
 }
